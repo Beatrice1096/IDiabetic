93fe5c6650f572a3b00c18a39a90a6a2b014a691
BeatriceElenaVizitiu <beatrice.vizitiu96@yahoo.com>
2021-01-12 22:02:53 +0200

    -Login

---
+++ app/src/main/java/com/example/idiabetic/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.idiabetic.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.idiabetic.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
