d485827a99c815cae7d7538dfc8bb9724660d134
mihailsandovici <mihailsandovici@gmail.com>
2021-01-12 22:44:16 +0200

    add kotlin libraries

--- app/src/main/java/com/example/idiabetic/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/idiabetic/ui/login/LoginActivity.kt
@@ -3,10 +3,19 @@ package com.example.idiabetic.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.idiabetic.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_bnt.setOnClickListener {
+            // the method which should be call when login is pressed
+        }
+    }
+
+    private fun login(username: String, password: String) {
+
     }
 }
