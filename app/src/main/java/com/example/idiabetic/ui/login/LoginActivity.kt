5462401a4fc0a8a292c6415e9dbd90a6d628d619
mihailsandovici <mihailsandovici@gmail.com>
2021-01-13 21:29:15 +0200

    login 1st

--- app/src/main/java/com/example/idiabetic/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/idiabetic/ui/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.idiabetic.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.idiabetic.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -11,11 +12,16 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         login_bnt.setOnClickListener {
-            // the method which should be call when login is pressed
+            // the method which should be called when login is pressed
+            login(
+                    login_username.text.toString(),
+                    login_password.text.toString()
+            )
         }
     }
 
     private fun login(username: String, password: String) {
-
+        Toast.makeText(this, "$username and $password", Toast.LENGTH_LONG).show()
     }
+
 }
